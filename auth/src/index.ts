a754dc7d1fa904b253e421871722e0e540278e62
Ayrton <ayrton@kelda.no>
2021-04-11 22:29:49 +0200

    feat: add route handlers and error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,29 @@
 import express from 'express'
+import 'express-async-errors'
+
 import { json } from 'body-parser'
+
+import { currentUserRouter } from './routes/current-user'
+import { signInRouter } from './routes/signin'
+import { signOutRouter } from './routes/signout'
+import { signUpRouter } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.use(json())
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there! 🎉')
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
+
+app.all('*', async () => {
+  throw new NotFoundError()
 })
 
+app.use(errorHandler)
+
 app.listen(3000, () => {
-  console.log('Listening on port 3000! 💙')
+  console.log('Listening on port 3000! 💚')
 })
