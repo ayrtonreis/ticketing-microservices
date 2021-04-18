5e16e24372cdee5f7947b61d9f34d6db702151b3
Ayrton <ayrton@kelda.no>
2021-04-18 16:38:29 +0200

    feat: add comment on shared module error handler

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -6,6 +6,7 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
     return res.status(err.statusCode).send({errors: err.serializeErrors() })
   }
 
+  // if the error is unknown, log it
   console.error(err)
 
   res.status(400).send({
